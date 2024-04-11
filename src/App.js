2dfe966f5b72ddb3e257a541c6f081f1ec4691ae
Santiago Santos <santiagosantos@Santiagos-MacBook-Pro-2.local>
2024-04-10 19:38:27 -0600

    JUE1.3 Rubrica, Badges y Calificar a Rubrica

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Inicio from './Inicio';
 import Juez from './Juez'; 
 import Admin from './Admin'; 
 import Login from './Login'; 
+import Rubrica from './Rubrica'; 
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/admin" element={<Admin />} />
         <Route path="/juez" element={<Juez />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/rubrica" element={<Rubrica />} />
         {/* Puedes agregar más rutas aquí para tus otras páginas */}
       </Routes>
     </div>
--- src/components/CardJuez.js
+++ src/components/CardJuez.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importa el componente Link de react-router-dom
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Importar el JavaScript de Bootstrap
 import '../Juez.css';
+import Badge from './Badge'; // Importa el componente Badge
 
 function Card({ title, image, description }) {
   const truncateText = (text, limit) => {
@@ -29,13 +30,16 @@ function Card({ title, image, description }) {
   );
 }
 
-function CardCalif({ title, image, description }) {
+function CardCalif({ title, image, description, ubicacion, categoria, nivelDesarrollo, status }) {
   const truncateText = (text, limit) => {
     if (text.length <= limit) {
       return text;
     }
     return text.slice(0, limit) + '...';
   };
+
+  // Determinar el classname según el valor de status
+  const badgeClassName = status === "No calificado" ? "badge2" : "badge";
 
   return (
     <div className="card">
@@ -47,8 +51,16 @@ function CardCalif({ title, image, description }) {
         <p className="h3">{truncateText(title, 50)}</p>
         <p className="p">{truncateText(description, 100)}</p>
 
+        {/* Agrega badges para ubicación, categoría, nivel de desarrollo y estado */}
+        <div className="badges">
+          <Badge data={ubicacion} className={badgeClassName} />
+          <Badge data={categoria} className={badgeClassName} />
+          <Badge data={nivelDesarrollo} className={badgeClassName} />
+          <Badge data={status} className={badgeClassName} />
+        </div>
+
         <Link to="/inicio" className="btn">Ver Proyecto</Link>
-        <Link to="/inicio" className="btncalif">Calificar</Link>
+        <Link to="/rubrica" className="btncalif">Calificar</Link>
       </div>
     </div>
   );
@@ -56,9 +68,17 @@ function CardCalif({ title, image, description }) {
 
 export function Cardlist({ posts }) {
   const list = posts.map(post => 
-    <CardCalif title={post.title} date={post.image} image={post.image} description={post.description} key={post.id} />
+    <CardCalif
+      title={post.title}
+      image={post.image}
+      description={post.description}
+      ubicacion={post.ubicacion}
+      categoria={post.categoria}
+      nivelDesarrollo={post.nivelDesarrollo}
+      status={post.status}
+      key={post.id}
+    />
   )
-
 
   return (
     <>
