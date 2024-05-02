c768508659dd681cf6973d30625596d3d2ce20cb
Gerardo Deustúa <97950449+GerryDH2807@users.noreply.github.com>
2024-05-02 13:54:57 -0600

    Nuevos Avacnces 05/02/24

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
-import Navigation from './Navigation';
+
 import Inicio from './Inicio'; 
-import Juez from './Juez'; 
-import Admin from './Admin'; 
-import Login from './Login'; 
-import Rubrica from './Rubrica'; 
+
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Navigation />} />
-        <Route path="/inicio" element={<Inicio />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/juez" element={<Juez />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/rubrica" element={<Rubrica />} />
-        {/* Puedes agregar más rutas aquí para tus otras páginas */}
-      </Routes>
+      <Inicio></Inicio>
     </div>
   );
 }
