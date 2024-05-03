34ce5416d22b5934ceba4aea19af3fe407738117
Sarai Santiago Lozano <128643905+SaraiSLoz@users.noreply.github.com>
2024-05-02 23:28:03 -0600

    Add files via upload

---
+++ src/pages/Main/Catalogue/Actual.js
@@ -0,0 +1,306 @@
+import './Actual.css';
+
+import { useState } from 'react';
+
+import CardConcept from '../../../img/CardConcept.png';
+import CardProto from '../../../img/CardProto.png';
+import CardFinish from '../../../img/CardFinish.png';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import Modal from 'react-bootstrap/Modal';
+
+import Menu from '../../../components/Menu/menu.js';
+
+function MyVerticallyCenteredModal({ TitleDetailed, DescriptionDetailed, TeacherDetailed, MemebersDetailed, DriveLink, YoutubeLink, ...props }) {
+const { CategoCheckModal } = props;
+
+return (
+    <Modal
+    {...props}
+    size="xl"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+    >
+    <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter" className='Titulo123'>
+            {TitleDetailed}
+        </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        <div className='container-fluid scroll-container1'>
+            <div className='row'>
+                {CategoCheckModal === "Concepto" && (
+                    <div className='col p-3 m-2 ConceptDetailedImage'>
+
+                    </div>
+                )}
+
+                {CategoCheckModal === "Prototipo" && (
+                    <div className='col p-3 m-2 ProtoDetailedImage'>
+
+                    </div>
+                )}
+
+                {CategoCheckModal === "Finalizado" && (
+                    <div className='col p-3 m-2 FinishDetailedImage'>
+
+                    </div>
+                )}
+
+                <div className='col-3 p-3 m-2 ContDetails'>
+                    <p className='Titulo123'>Miembros del equipo: </p> <span>{MemebersDetailed}</span>
+                </div>
+                <div className='col-3 p-3 m-2 ContDetails'>
+                    <p className='Titulo123'>Profesor(es):</p> <span>{TeacherDetailed}</span> 
+                </div>
+            </div>
+
+
+            <div className='row p-2'>
+                <div className='col p-4 ContDetails'>
+                    <div className='container-fluid'>
+
+                        <div className='row'>
+                            <div className='col'>
+                                <h4 className='Titulo123'>Descripción del proyecto</h4>
+                            </div>
+                        </div>
+
+                        <div className='row'>
+                            <div className='col'>
+                                <span className='TextoCardWrap'>{DescriptionDetailed}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='row p-2'>
+                <div className='col p-2 ContDetails'>
+                    <div className='container-fluid '>
+                        <div className='row'>
+                            <div className='col p-1 m-2 '>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <div className='col text-center'>
+                                            <a href={DriveLink}><i className='bi bi-filetype-pdf ModalIconsCurrent'></i></a>
+                                        </div>
+                                    </div>
+
+                                    <div className='row'>
+                                        <div className='col text-center'>
+                                            <span className='Subtitulo'>Ver el cartel del proyecto</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className='col p-1 m-2'>
+                                <div className='container'>
+                                    <div className='row'>
+                                        <div className='col text-center'>
+                                            <a href={YoutubeLink}><i className='bi bi-youtube ModalIconsCurrent'></i></a>
+                                        </div>
+                                    </div>
+
+                                    <div className='row'>
+                                        <div className='col text-center'>
+                                            <span className='Subtitulo'>Ver el video explicativo del video</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Modal.Body>
+    <Modal.Footer>
+        <Button onClick={props.onHide} className='ButtonColors123'>Cerrar</Button>
+    </Modal.Footer>
+    </Modal>
+);
+}
+
+function ButtonModal({ CategoCheckButton, TitleDetailed, DescriptionDetailed }) {
+    const [modalShow, setModalShow] = useState(false);
+  
+    return (
+      <>
+        <Button className='ButtonColors' onClick={() => setModalShow(true)}>
+          Ver proyecto
+        </Button>
+  
+        <MyVerticallyCenteredModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          CategoCheckModal={CategoCheckButton}
+          TitleDetailed={TitleDetailed}
+          DescriptionDetailed={DescriptionDetailed}
+          TeacherDetailed={'Daniela Lozada Bracamontes'}
+
+          MemebersDetailed={'Gerardo Deustúa Hernández' }
+
+          DriveLink={'http://google.com'}
+          YoutubeLink={'http://youtube.com'}
+        />
+      </>
+    );
+  }
+  
+
+
+function CardProj({ CategoCheck, Title, Description }) {
+    return (
+    <div className='col p-3'>
+        {CategoCheck === "Concepto" && (
+            <Card style={{ width: '18rem', height: '90%' }} className='CardsInfoContainer'>
+                <Card.Img variant="top" src={CardConcept} className='FotoCard' />
+                <Card.Body>
+                <Card.Title className='Titulo123'>{Title}</Card.Title>
+                <Card.Text className='TextoCardWrap'>{Description}</Card.Text>
+                <center>
+                    <ButtonModal CategoCheckButton={CategoCheck} TitleDetailed={Title} DescriptionDetailed={Description}>Ver proyecto</ButtonModal>
+                </center>
+                </Card.Body>
+            </Card>
+        )}
+
+        {CategoCheck === "Prototipo" && (
+            <Card style={{ width: '18rem' , height: '90%' }} className='CardsInfoContainer'>
+                <Card.Img variant="top" src={CardProto} className='FotoCard' />
+                <Card.Body>
+                <Card.Title className='Titulo123'>{Title}</Card.Title>
+                <Card.Text className='TextoCardWrap'>{Description}</Card.Text>
+                <center>
+                    <ButtonModal CategoCheckButton={CategoCheck} TitleDetailed={Title} DescriptionDetailed={Description}>Ver proyecto</ButtonModal>
+                </center>
+                </Card.Body>
+            </Card>
+        )}
+
+        {CategoCheck === "Finalizado" && (
+            <Card style={{ width: '18rem', height: '90%' }} className='CardsInfoContainer'>
+                <Card.Img variant="top" src={CardFinish} className='FotoCard' />
+                <Card.Body>
+                <Card.Title className='Titulo123'>{Title}</Card.Title>
+                <Card.Text className='TextoCardWrap'>{Description}</Card.Text>
+                
+                <center>
+                    <ButtonModal CategoCheckButton={CategoCheck} TitleDetailed={Title} DescriptionDetailed={Description}>Ver proyecto</ButtonModal>
+                </center>
+                </Card.Body>
+            </Card>
+        )}
+
+
+    </div>  
+    );
+}
+
+export default function Actual() {
+    return (
+      <>
+      <Menu />
+        <div className='container-fluid'>
+
+            <div className='row m-4 p-3 CardsContainer'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col'>
+                            <h3 className='Titulo123'>Bienvenido a los proyectos actuales</h3>
+                        </div>
+                    </div>
+
+                    <div className='row'>
+                        <div className='col'>
+                            <p className='Texto'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec purus ut nunc molestie rhoncus. Quisque et facilisis elit. Aliquam in ante rhoncus, fermentum diam vel, efficitur neque. Nam sapien nulla, sodales et massa sed, luctus tincidunt mauris. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent eget egestas nibh, vel rhoncus mi. Mauris volutpat eu nisi nec molestie. Etiam non leo tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla dolor augue, faucibus in ex a, vehicula rutrum tortor. Aenean tempus ligula eget sem tempus finibus. Duis sit amet velit interdum, vulputate sem et, vestibulum magna. Nullam suscipit enim sapien, at accumsan tellus gravida nec. Proin lectus dui, faucibus finibus metus quis, pulvinar dapibus mauris. Donec vehicula nisi vel viverra malesuada.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='row m-4 p-3 CardsContainer'>
+
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <div className='col'>
+                            <h3 className='Titulo123'>Concepto</h3>
+                        </div>
+                    </div>
+
+                    <div className='row scroll-container'>
+                        <div className='col'>
+                            <div className="d-flex flex-nowrap">
+                                <CardProj CategoCheck={'Finalizado'} Title={"Titulo del proyecto1"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Finalizado'} Title={"Titulo del proyecto2"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Prototipo'} Title={"Titulo del proyecto3"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Concepto'} Title={"Titulo del proyecto4"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Prototipo'} Title={"Titulo del proyecto5"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Concepto'} Title={"Titulo del proyecto6"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Concepto'} Title={"Titulo del proyecto7"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Prototipo'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Prototipo'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Concepto'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Finalizado'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Finalizado'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>  
+                            </div>                  
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+  
+            <div className='row m-4 p-3 CardsContainer'>
+
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <div className='col'>
+                            <h3 className='Titulo123'>Prototipo</h3>
+                        </div>
+                    </div>
+
+                    <div className='row scroll-container'>
+                        <div className='col'>
+                            <div className="d-flex flex-nowrap">
+                                <CardProj CategoCheck={'Prototipo'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Concepto'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Finalizado'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Finalizado'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                            </div>                  
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+  
+            <div className='row m-4 p-3 CardsContainer'>
+
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <div className='col'>
+                            <h3 className='Titulo123'>Prototipo terminado</h3>
+                        </div>
+                    </div>
+
+                    <div className='row scroll-container'>
+                        <div className='col'>
+                            <div className="d-flex flex-nowrap">
+                                <CardProj CategoCheck={'Finalizado'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Prototipo'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Concepto'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+                                <CardProj CategoCheck={'Finalizado'} Title={"Titulo del proyecto"} Description={"asafasfafafaaskfbaksfbaksbfaksbfsaskfbakfbakfbaskfaskfbaskfasbkfasbfkbkfasbfiasbfkasbfksbfkasfbaskfbaskfbskfbkfbaskfbaskfbskfbsfk"}/>
+
+                            </div>                  
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+      </>
+    );
+  }
---
+++ src/pages/Student/ProjSelection/ProjSelection.js
@@ -0,0 +1,121 @@
+import { Link } from 'react-router-dom';
+
+import Badge from './Badge';
+import './Juez.css';
+import './Badge.css';
+
+import './ProjSelection.css';
+
+import ToggleBarStudent from '../../../components/TogglebarStudent/togglebarStudent.js';
+
+function CardCalif({ title, description, categoria, id_Proyecto, status }) {
+    const truncateText = (text, limit) => {
+        if (!text || typeof text !== 'string' || text.length <= limit) {
+          return text;
+        }
+        return text.slice(0, limit) + '...';
+    };
+  
+
+    return (
+        <>
+            <div className='col-auto p-3'>
+
+                <div className="card mb-1 me-0">
+
+                    {categoria === 'Concepto' && (
+
+                        <div className="imag algoimagConcept">
+                                    
+                        </div>
+
+                    )}
+
+                    {categoria === 'Prototipo' && (
+
+                        <div className="imag algoimagProto">
+                                    
+                        </div>
+
+                    )}
+
+                    {categoria === 'Prototipo finalizado' && (
+
+                        <div className="imag algoimagFinish">
+                                    
+                        </div>
+
+                    )}
+
+            
+                    <div className="text">
+                        <p className="h3">{truncateText(title, 50)}</p>
+                        <p className="p">{truncateText(description, 100)}</p>
+                
+                        <div className="badge-container">
+                            <Badge data={categoria} className="badge text-wrap" />
+                            <Badge data={id_Proyecto} className="badge" />
+                            
+                            {status === "Rechazado" && (
+                                <div className="badge-container">
+                                    <div className="badge2">{status}</div>
+                                </div>
+                            )}
+
+                            {status === "Aceptado" && (
+                                <div className="badge-container">
+                                    <div className="badge3">{status}</div>
+                                </div>
+                            )}
+
+                        </div>
+                
+                        <Link to="/resumen-proyecto-estudiante" className="btn23">Ver Proyecto</Link>
+                        
+                    </div>
+                </div>
+            </div>    
+        </>
+    );
+  }
+  
+
+export default function ProjSelection(){
+    return(
+
+        <>
+            <ToggleBarStudent />
+            <div className='container-fluid centered-containerProjSelc'>
+                <div className='row m-4 p-3 TitleSelectContainer'>
+                    <div className='col '>
+                        <h1 className='TituloProjSEL text-center'>Proyectos en los que participas</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container-fluid'>
+                <div className='row d-flex flex-col justify-content-evenly'>
+
+                    <CardCalif 
+                        title={'Robot automata para automatizar automatas'}
+                        description="Robot Automata para Automatizar Autómatas  es un proyecto innovador para desarrollar un sistema robótico que automatiza tareas complejas en la industria. Utiliza algoritmos avanzados de inteligencia artificial y aprendizaje automático para aumentar la eficiencia y precisión en la producción, optimizando recursos."
+                        categoria={"Prototipo"}
+                        id_Proyecto={"NPF103"}
+                        status={"Aceptado"}
+                    />
+            
+                    <CardCalif 
+                        title={'Maquina perpetua que alimenta maquinas perpetuas'}
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                        categoria={"Concepto"}
+                        id_Proyecto={"CP543"}
+                        status={"Rechazado"}
+                    />
+
+
+                </div>
+            </div>        
+        </>
+
+    );
+}
