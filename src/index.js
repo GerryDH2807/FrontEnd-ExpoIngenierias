05202025d6c562fdcb0f9bc815547dcc73017a0c
Sarai Santiago Lozano <sarailoza05@gmail.com>
2024-05-15 20:05:20 -0600

    Teacher resume project fixed

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ root.render(
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin}
+      redirectUri= "http://localhost:3000/Callback"
     >
       <BrowserRouter>
         <App />
