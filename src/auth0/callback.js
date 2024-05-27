34ad391831607d88db09d7539cb2783232d3ee94
MarlonY123 <A01424875@tec.mx>
2024-05-26 22:05:53 -0600

    merge

--- src/auth0/callback.js
+++ src/auth0/callback.js
@@ -22,7 +22,7 @@ function Callback() {
       const username = user.email.split('@')[0];
       const isStudent = /^[aA]\d{8}$/.test(username);
 
-      if (false) {
+      if (isStudent) {
         localStorage.setItem('userRole', 'student');
         navigate('/principal-estudiante'); // Redirigir a la página de estudiante
       } else {
